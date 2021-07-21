33d67b224defa10e3f38835f4ef6c70f1ce4b8d4
GuillaumeMontassier <52446347+GuillaumeMontassier@users.noreply.github.com>
2021-07-21 17:45:44 -0400

    last changes about the navbar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Navbar from "./component/navbar";
 import Project from "./component/project";
 import Skills from "./component/skills";
 import "./App.css";
-import Example from "./component/navbar2";
 
 function App() {
   return (
     // <div className="bg-gradient-to-r from-green-400 to-blue-500 body-font">
     <div className="text-gray-500 bg-black body-font">
-      {/* <Navbar/> */}
-      <Example />
+      <Navbar />
       <About />
       <About />
       <About />
--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,42 +1,117 @@
-import React from "react";
-import { GitHub, Linkedin } from "react-feather";
+import { Disclosure } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { ExternalLink, GitHub, Linkedin } from "react-feather";
+
+const navigation = [
+  { name: "About me", href: "#", current: true },
+  { name: "Studies", href: "#", current: false },
+  { name: "Projects", href: "#", current: false },
+  { name: "Contact me", href: "#", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function Navbar() {
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-10">
-      <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
-        <a className="font-medium text-white mb-4 md:mb-0 ml-3 text-xl">
-          Guillaume Montassier
-        </a>
-        <nav className=" md:ml-auto md:py-1 md:pl-4	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
-            A propos
-          </a>
-          <a href="#projects" className="mr-5 hover:text-white">
-            Compétences
-          </a>
-          <a href="#projects" className="mr-5 hover:text-white">
-            Projets
-          </a>
-          <a href="#skills" className="mr-5 hover:text-white">
-            Contact
-          </a>
-          <a
-            href="https://github.com/GuillaumeMontassier"
-            target="_blank"
-            className="mr-5 hover:text-purple-500"
-          >
-            <GitHub strokeWidth={1.5} />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/guillaume-montassier-6284a5152/"
-            target="_blank"
-            className="mr-5 hover:text-linkedin"
-          >
-            <Linkedin strokeWidth={1.5} />
-          </a>
-        </nav>
-      </div>
-    </header>
+    <Disclosure as="nav" className="bg-transparent">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="flex-2 flex items-center justify-center sm:items-stretch">
+                {/* Title section */}
+                <div className="flex-shrink-0 flex items-center text-xl antialiased font-semibold">
+                  Guillaume Montassier
+                </div>
+              </div>
+              {/* Tab section */}
+              <div className=" flex-end inset-y-0 right-0 flex items-start pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div className="hidden md:block items-center">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        item.current
+                          ? "text-white"
+                          : "text-gray-500 hover:bg-gray-800 hover:text-white",
+                        "px-3 py-2 rounded-md text-sm font-medium inline-block"
+                      )}
+                      aria-current={item.current ? "page" : undefined}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                  <a
+                    href="https://github.com/GuillaumeMontassier"
+                    target="_blank"
+                    className="px-3 py-2 text-gray-500 hover:bg-gray-800 hover:text-purple-500 rounded-md inline-block align-middle"
+                  >
+                    <GitHub strokeWidth={1.5} />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/guillaume-montassier-6284a5152/"
+                    target="_blank"
+                    className="px-3 py-2 text-gray-500 hover:bg-gray-800 hover:text-linkedin rounded-md inline-block align-middle"
+                  >
+                    <Linkedin strokeWidth={1.5} />
+                  </a>
+                </div>
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="sm:hidden inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
+          </div>
+          {/* Mobile poped-up menu */}
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium "
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </a>
+              ))}
+              <a
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium text-justify"
+                href="https://github.com/GuillaumeMontassier"
+              >
+                <div className="flex">
+                  <p className="flex-1">GitHub</p>
+                  <ExternalLink strokeWidth={1.5} className="flex-2" />
+                </div>
+              </a>
+              <a
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium text-justify"
+                target="_blank"
+                href="https://www.linkedin.com/in/guillaume-montassier-6284a5152/"
+              >
+                <div className="flex">
+                  <p className="flex-1">LinkedIn</p>
+                  <ExternalLink strokeWidth={1.5} className="flex-2" />
+                </div>
+              </a>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 }
